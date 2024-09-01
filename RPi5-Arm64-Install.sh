bcd88bd4aeed535fae3071f5b7298501957595a2
Sakakibara <h11404m@gmail.com>
2024-09-01 19:52:38 +0900

    Update RPi5-Arm64-Install.sh

--- RPi5-Arm64-Install.sh
+++ RPi5-Arm64-Install.sh
@@ -31,7 +31,7 @@ iface vmbr0 inet static
 echo 'deb [arch=arm64] https://mirrors.apqa.cn/proxmox/debian/pve bookworm port'>/etc/apt/sources.list.d/pveport.list
 
 
-curl https://mirrors.apqa.cn/proxmox/debian/pveport.gpg -o /etc/apt/trusted.gpg.d/pveport.gpg 
+curl -L https://mirrors.apqa.cn/proxmox/debian/pveport.gpg -o /etc/apt/trusted.gpg.d/pveport.gpg 
 
 apt update && apt full-upgrade
 
